a99b685396a814070619ad5cad62cc57312e4f6b
Jcodefox <jcodefox@gmail.com>
2023-02-07 00:39:05 -0500

    Version 7

--- build/deploy.fish
+++ build/deploy.fish
@@ -5,3 +5,6 @@ math (cat version.txt) + 1 > version.txt
 butler push --userversion-file=version.txt html jcodefox/hatchetflashprototypegodot:html-universal-prototype
 butler push --userversion-file=version.txt windows jcodefox/hatchetflashprototypegodot:windows-universal-prototype
 butler push --userversion-file=version.txt linux jcodefox/hatchetflashprototypegodot:linux-universal-prototype
+
+git add *
+git commit -m (string join ' ' Version (cat version.txt))
